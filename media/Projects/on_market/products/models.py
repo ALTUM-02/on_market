a01749d93f1327e26862897a400566f071fd8862
ALTUM-02 <martin249jesus@gmail.com>
2026-04-12 17:58:53 +0300

    new change

--- media/Projects/on_market/products/models.py
+++ media/Projects/on_market/products/models.py
@@ -8,3 +8,18 @@ class Product(models.Model):
     description = models.TextField()
     Stock = models.IntegerField()
     image = models.ImageField(upload_to='products/')
+    
+    def _str_(self):
+        return self.name
+    
+    
+class Cart(models.Model):
+    session_id = models.CharField(max_length=255)
+    
+    def _str_(self):
+        return self.session_id
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    Product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quality = models.IntegerField(default=1)       
