95275f08550eba3e5b7f6895050e491fe8b32676
ALTUM-02 <martin249jesus@gmail.com>
2026-04-14 23:03:45 +0300

    new change

--- media/Projects/on_market/users/views.py
+++ media/Projects/on_market/users/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 from rest_framework.decorators import api_view
