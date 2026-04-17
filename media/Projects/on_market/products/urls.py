737634270eb9f3166a65dcc410035f70cfb549e0
ALTUM-02 <martin249jesus@gmail.com>
2026-04-17 15:49:08 +0300

    new change

--- media/Projects/on_market/products/urls.py
+++ media/Projects/on_market/products/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('product/<int:id>/', views.product_detail, name='product_detail'),
+    path('add-product/', views.add_product, name='add_product'),
 ]
