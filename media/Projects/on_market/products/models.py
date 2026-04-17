f93997c12e2377425f9e4e937937cb53745873d4
ALTUM-02 <martin249jesus@gmail.com>
2026-04-17 16:58:05 +0300

    new change

--- media/Projects/on_market/products/models.py
+++ media/Projects/on_market/products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     description = models.TextField()
-    Stock = models.IntegerField(null=True, blank=True)
+    Stock = models.IntegerField(default=0)
     image = models.ImageField(upload_to='products/')
     
     def _str_(self):
