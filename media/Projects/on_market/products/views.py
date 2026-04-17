20a926b2df2839ce51fca0947ce383197ec191fa
ALTUM-02 <martin249jesus@gmail.com>
2026-04-17 15:35:11 +0300

    new change

--- media/Projects/on_market/products/views.py
+++ media/Projects/on_market/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 from .models import Product
