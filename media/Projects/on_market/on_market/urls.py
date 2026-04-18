586863f714c0c5343a0aff376c6d647abd3fd51a
ALTUM-02 <martin249jesus@gmail.com>
2026-04-18 15:01:51 +0300

    new change

--- media/Projects/on_market/on_market/urls.py
+++ media/Projects/on_market/on_market/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include 
 from products.views import home
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 from django.conf.urls.static import static
 #from .views import home
 
@@ -48,5 +49,5 @@ urlpatterns = [
     path('api/refresh/', TokenRefreshView.as_view()),
 ]
 
-#if settings.DEBUG:
-    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#
