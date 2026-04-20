d8a3b382c3d33bf541c6bc6ca487cde753833468
ALTUM-02 <martin249jesus@gmail.com>
2026-04-20 10:31:06 +0300

    new change

--- media/Projects/on_market/users/urls.py
+++ media/Projects/on_market/users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .view
+from .views import api_login
 from . import views
 
 urlpatterns = [
-    path('login', views.login_view, name='login'),
+    path('api/login/', api_login, name='api_login'),
     path('logout/', views.logout_view, name='logout'),
 ]
