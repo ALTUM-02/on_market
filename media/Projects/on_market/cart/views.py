d262e262f24e253d16cf59d58cc45a6c66055e98
ALTUM-02 <martin249jesus@gmail.com>
2026-04-24 17:28:28 +0300

    new change

--- media/Projects/on_market/cart/views.py
+++ media/Projects/on_market/cart/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.decorators import login_required
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Cart, CartItem
+from products.models import Product
 from .serializers import CartSerializer
+import json
 from django.http import JsonResponse
 
 def home(request):
