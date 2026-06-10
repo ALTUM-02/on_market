8b72ebd30ad94bc6247b6b3870e3c67c9a47c3f7
ALTUM-02 <martin249jesus@gmail.com>
2026-06-10 06:33:47 +0300

    new change

--- media/Projects/e_learning-project/backend/config/authentication/views.py
+++ media/Projects/e_learning-project/backend/config/authentication/views.py
@@ -1,12 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework import  IsAuthenticated   
+from rest_framework_simplejwt.tokens import RefreshToken 
 
 
 class RegisterView(APIView):
