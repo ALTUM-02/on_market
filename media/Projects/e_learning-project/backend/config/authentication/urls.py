d1bfe4d9e05b0db3e996e134d44c002e3dcf3c34
ALTUM-02 <martin249jesus@gmail.com>
2026-06-10 05:24:52 +0300

    new chnage

--- media/Projects/e_learning-project/backend/config/authentication/urls.py
+++ media/Projects/e_learning-project/backend/config/authentication/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
-from .views import LoginView, RegisterView
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("login/", LoginView.as_view()),
-    path("register/", RegisterView.as_view()),
+    path("admin/", admin.site.urls),
+
+    path("api/auth/", include("authentication.urls")),
+    path("api/", include("api.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
